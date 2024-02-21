c83b175e326b5794e0c3de1e8ad1b67d8912393f
kusha2000 <kushanandarawewa1@gmail.com>
2024-02-21 12:03:21 +0530

    SignUp Screen Created

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:note_app_flutter/views/signInScreen.dart';
 
 void main() async {
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
--- lib/views/signUpScreen.dart
+++ lib/views/signUpScreen.dart
@@ -2,23 +2,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
-import 'package:note_app_flutter/views/signUpScreen.dart';
+import 'package:note_app_flutter/views/signInScreen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         centerTitle: true,
-        title: const Text("Login Screen"),
+        title: const Text("SignUp Screen"),
         // actions: const [Icon(Icons.more_vert)],
       ),
       body: SingleChildScrollView(
@@ -27,10 +26,30 @@ class _LoginScreenState extends State<LoginScreen> {
             children: [
               Container(
                 alignment: Alignment.center,
-                height: 250,
-                child: Lottie.asset("assets/Animation - 1708493774305.json"),
+                height: 200,
+                child: Lottie.asset("assets/Animation - 1708497028538.json"),
               ),
-              const SizedBox(height: 50.0),
+              const SizedBox(height: 20.0),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      hintText: "UserName",
+                      enabledBorder: OutlineInputBorder()),
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.phone),
+                      hintText: "Phone",
+                      enabledBorder: OutlineInputBorder()),
+                ),
+              ),
+              const SizedBox(height: 10.0),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 30.0),
                 child: TextFormField(
@@ -52,24 +71,16 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               const SizedBox(height: 15.0),
-              ElevatedButton(onPressed: () {}, child: Text("Login")),
-              const SizedBox(height: 10.0),
-              Container(
-                child: const Card(
-                    child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text("Forgot Password"),
-                )),
-              ),
+              ElevatedButton(onPressed: () {}, child: Text("Sign Up")),
               GestureDetector(
                 onTap: () {
-                  Get.to(() => SignUpScreen());
+                  Get.to(() => LoginScreen());
                 },
                 child: Container(
                   child: const Card(
                       child: Padding(
                     padding: EdgeInsets.all(10.0),
-                    child: Text("Don't have an account SignUp"),
+                    child: Text("Already have an account LoginIn"),
                   )),
                 ),
               ),
